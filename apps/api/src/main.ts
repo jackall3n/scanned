0e9cc4956d8443d6b021c85351d7eeeb0995993a
Jack Allen <jackallensemail+git@gmail.com>
2021-12-16 12:32:32 +0000

    cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,17 +10,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
 
-  app.setGlobalPrefix(globalPrefix);
   app.enableCors();
 
   const port = process.env.PORT || 5555;
   await app.listen(port);
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 API is running on: http://localhost:${port}`);
 }
 
 bootstrap();
